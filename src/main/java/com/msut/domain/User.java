5e6f471a94e5027d81b1396d48faab2a7c61687a
msut <sariuszm@gmail.com>
2017-02-07 20:41:41 +0100

    add equals and hashcode

--- src/main/java/com/msut/domain/User.java
+++ src/main/java/com/msut/domain/User.java
@@ -54,4 +54,19 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return username.hashCode();
+    }
 }
