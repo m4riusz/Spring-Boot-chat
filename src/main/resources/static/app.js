16a0ea5de846189bc078581eaa80e03f6978b7a5
msut <sariuszm@gmail.com>
2017-02-07 20:39:07 +0100

    add basic view

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,63 @@
+/**
+ * Created by mariusz on 05.02.17.
+ */
+
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+
+        stompClient.subscribe('/app/users.all', function (response) {
+            $("#users").append("<li>" + JSON.parse(response.body).username + "</li>");
+        });
+        stompClient.send('/app/users.new', {}, JSON.stringify({}));
+
+    });
+}
+
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/message", {}, JSON.stringify({'text': $("#name").val()}));
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $("#connect").click(function () {
+        connect();
+    });
+    $("#disconnect").click(function () {
+        disconnect();
+    });
+    $("#send").click(function () {
+        sendName();
+    });
+});
