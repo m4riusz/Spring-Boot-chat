d0742785ccd97465b3ad8ef1e164840c0e38cc05
msut <sariuszm@gmail.com>
2017-02-11 16:17:32 +0100

    add validation constraints

--- src/main/java/com/msut/dto/NewMessageDto.java
+++ src/main/java/com/msut/dto/NewMessageDto.java
@@ -1,10 +1,16 @@
 package com.msut.dto;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
 /**
  * Created by mariusz on 10.02.17.
  */
 public class NewMessageDto {
 
+    @NotBlank(message = "Message can't be empty!")
+    @Length(min = 1, max = 255,
+            message = "Message length should be between {min} and {max}!")
     private String content;
 
 
